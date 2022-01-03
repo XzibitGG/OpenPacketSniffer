e26143a2e761282136d9ce629ee59cd7bfb1407a
Xzibit <neeljos24@gmail.com>
2022-01-02 22:57:26 -0500

    Use Source address

--- src/main/kotlin/org/openpacketsniffer/packethandlers/ipPacketHandler.kt
+++ src/main/kotlin/org/openpacketsniffer/packethandlers/ipPacketHandler.kt
@@ -1,25 +1,19 @@
 package org.openpacketsniffer.packethandlers
-
-import org.openpacketsniffer.PacketListener
 import org.openpacketsniffer.SnifferStats.addPacketStat
 import org.pcap4j.packet.IpV4Packet
 import org.pcap4j.packet.IpV6Packet
-import org.pcap4j.packet.Packet
-import java.net.InetAddress
+
 
 class IpPacketHandler(){
     companion object {
 
         fun handle(packet : IpV4Packet){
-            (packet.header.dstAddr).let {
-                if (it != InetAddress.getLocalHost()) addPacketStat(it.hostAddress)
-            }
+            addPacketStat(packet.header.srcAddr.hostAddress)
+
         }
 
         fun handle(packet : IpV6Packet){
-            (packet.header.dstAddr).let {
-                if (it != InetAddress.getLocalHost()) addPacketStat(it.hostAddress)
-            }
+            addPacketStat(packet.header.srcAddr.hostAddress)
         }
 
 
